43b05cbb20c0ffe94d84453b80c89d1ef6c2ead9
zakriaaliyar12345-wq <zakriaaliyar12345@gmail.com>
2026-08-19 20:37:43 +0430

    last

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -43,10 +43,10 @@ export default async function BlogPage() {
 }
 
 async function LoadBlogList() {
-  // "use cache";
+  "use cache";
 
-  // cacheLife("hours");
-  // cacheTag("blog");
+  cacheLife("hours");
+  cacheTag("blog");
   await connection();
 
   await new Promise((resolve) => setTimeout(resolve, 5000));
